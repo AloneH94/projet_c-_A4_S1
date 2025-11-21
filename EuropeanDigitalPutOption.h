6ff45edae953fc9288942927e5b3f50469cb4058
vincent-guerin <81231369+vincent-guerin@users.noreply.github.com>
2025-11-21 14:24:27 +0100

    Delete EuropeanDigitalPutOption.h

--- EuropeanDigitalPutOption.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EuropeanVanillaOption.h"
-
-class EuropeanDigitalPutOption : public EuropeanVanillaOption {
-public:
-    EuropeanDigitalPutOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
-    double payoff(double spot) const override { return (spot <= _strike) ? 1.0 : 0.0; }
-    OptionType getOptionType() const override { return OptionType::Put; }
-};
