476cb9f54707247e46eb3d0d96948e73ff19be74
vincent-guerin <81231369+vincent-guerin@users.noreply.github.com>
2025-11-21 14:22:10 +0100

    Delete AmericanOption.h

--- AmericanOption.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Option.h"
-
-class AmericanOption : public Option {
-public:
-    AmericanOption(double expiry) : Option(expiry) {}
-    bool isAmericanOption() const override { return true; }
-};
