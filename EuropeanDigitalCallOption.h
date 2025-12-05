a95ae142209579d77328885f966eda8d67e0a63a
vincent-guerin <81231369+vincent-guerin@users.noreply.github.com>
2025-12-05 14:54:41 +0100

    Delete EuropeanDigitalCallOption.h

--- EuropeanDigitalCallOption.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EuropeanVanillaOption.h"
-
-class EuropeanDigitalCallOption : public EuropeanVanillaOption {
-public:
-    EuropeanDigitalCallOption(double expiry, double strike) : EuropeanVanillaOption(expiry, strike) {}
-    double payoff(double spot) const override { return (spot >= _strike) ? 1.0 : 0.0; }
-    OptionType getOptionType() const override { return OptionType::Call; }
-};
